741c0223c42114faad9947d04e8727b5cb10f27a
Jona <jon_ra@hotmail.es>
2018-06-25 18:26:28 -0500

    añadir documentos

---
+++ index.php
@@ -0,0 +1,54 @@
+
+<?php 
+	session_start(); 
+
+	//https://codewithawa.com/posts/complete-user-registration-system-using-php-and-mysql-database
+	
+	if (!isset($_SESSION['username'])) {
+		$_SESSION['msg'] = "You must log in first";
+		header('location: login.php');
+	}
+
+	if (isset($_GET['logout'])) {
+		session_destroy();
+		unset($_SESSION['username']);
+		header("location: login.php");
+	}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bienvenido</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="header">
+		<h2>Página principal</h2>
+	</div>
+	<div class="content">
+
+		<!-- notification message -->
+		<?php if (isset($_SESSION['success'])) : ?>
+			<div class="error success" >
+				<h3>
+					<?php 
+						echo $_SESSION['success']; 
+						unset($_SESSION['success']);
+					?>
+				</h3>
+			</div>
+		<?php endif ?>
+
+		<!-- logged in user information -->
+		<?php  if (isset($_SESSION['username'])) : ?>
+			<p>Bienvenido <strong><?php echo $_SESSION['username']; ?></strong></p>
+			<img src="images/a1.jpg" alt="chip" width="400" height="200">
+			<p> <a href="index.php?logout='1'" style="color: red;">Salir</a> </p>
+		<?php endif ?>
+
+		
+	</div>
+		
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,36 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sistema de Registro WAMP</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<div class="header">
+		<h2>Ingreso</h2>
+	</div>
+	
+	<form method="post" action="login.php">
+
+		<?php include('errors.php'); ?>
+
+		<div class="input-group">
+			<label>Nombre de usuario</label>
+			<input type="text" name="username" >
+		</div>
+		<div class="input-group">
+			<label>Contraseña</label>
+			<input type="password" name="password">
+		</div>
+		<div class="input-group">
+			<button type="submit" class="btn" name="login_user">Login</button>
+		</div>
+		<p>
+			Aún no eres miembro? <a href="register.php">Registrate</a>
+		</p>
+	</form>
+
+
+</body>
+</html>
